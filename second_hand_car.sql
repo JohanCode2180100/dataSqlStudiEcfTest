2a6ff650b5b2dc0f61b44ea2a76db86681add592
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-09-21 11:00:26 +0200

    add data set

---
+++ second_hand_car.sql
@@ -0,0 +1,13 @@
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 21 sep. 2023 à 10:57
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 7.4.29
+
+JEU DE DONNEES A INSERER EN BASE DE DONNEES 
+
+INSERT INTO `second_hand_car` (`Second_hand_car_id`, `Brand`, `Name`, `Year`, `Price`, `Kilometer`, `Picture`, `Description`, `CreatedAt`, `Admin_id`) VALUES
+(1, 'CHRYSLER', 'PLYMOUTH', 1969, 145967, 67000, './assets/picture/plymouth.jpg', NULL, NULL, NULL),
+(2, 'FORD', 'GT40', 2005, 13756, 399000, './assets/picture/gt40.jpg', NULL, NULL, NULL),
+(3, 'AUDI', 'A3 Sportback', 2021, 23600, 31990, './assets/picture/audi.jpg', NULL, NULL, NULL),
+(4, 'MERCEDES', 'CSL COUPE', 2018, 48000, 61987, './assets/picture/mercedes.jpg', NULL, NULL, NULL);
+
